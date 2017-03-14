93282738ab8fd1b9e7e99b6f66f20ad579835377
apecr <albertoeyo@outlook.es>
2017-03-14 10:41:01 +0100

    Añadimos SUT
    Incluimos la clase que probara el test

---
+++ diseño-agil-con-tdd/disenho-agil-con-tdd-code/src/main/java/com/carlos/ble/tdd/ejemplos/nunit/NameNormalizer.java
@@ -0,0 +1,15 @@
+package com.carlos.ble.tdd.ejemplos.nunit;
+
+public class NameNormalizer {
+
+	public String firstLetterUpperCase(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String surnameFirst(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- diseño-agil-con-tdd/disenho-agil-con-tdd-code/src/test/java/com/carlos/ble/tdd/ejemplos/nunit/NameNormalizerTest.java
+++ diseño-agil-con-tdd/disenho-agil-con-tdd-code/src/test/java/com/carlos/ble/tdd/ejemplos/nunit/NameNormalizerTest.java
@@ -2,17 +2,35 @@ package com.carlos.ble.tdd.ejemplos.nunit;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NameNormalizerTest {
+	
+	private NameNormalizer normalizer;
+
+	@Before
+	public void setUp(){
+		this.normalizer = new NameNormalizer();
+	}
+	
 	@Test
-	public void FirstLetterUpperCase() {
+	public void firstLetterUpperCase() {
 		// Arrange
 		String name = "pablo rodriguez";
-		NameNormalizer normalizer = new NameNormalizer();
 		// Act
-		String result = normalizer.FirstLetterUpperCase(name);
+		String result = this.normalizer.firstLetterUpperCase(name);
 		// Assert
 		assertEquals("Pablo Rodriguez", result);
+	}
+	
+	@Test
+	public void surnameFirst() {
+		// Arrange
+		String name = "gonzalo aller";
+		// Act
+		String result = this.normalizer.surnameFirst(name);
+		// Assert
+		assertEquals("aller, gonzalo", result);
 	}
 }
